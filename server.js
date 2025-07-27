259b7f64295f158e8aaa8c5433cfdd00a54e5dc5
LPZ8 <lpz_84@hotmail.com>
2025-07-27 12:22:37 +0200

    ejercicio proyecto final completado sergio y yeny

--- server.js
+++ server.js
@@ -21,4 +21,5 @@ app.delete('/api/items/:id', (req, res) => {
   res.status(204).send();
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
